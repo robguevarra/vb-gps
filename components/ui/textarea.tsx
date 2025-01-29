a41c1bf570b4f73d3e1e5858cf36276d06a7d8fe
robguevarra <robneil@gmail.com>
2025-01-29 22:25:51 +0800

    new branch

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,15 +1,14 @@
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
+import * as React from 'react'
+import { cn } from '@/lib/utils'
 
 const Textarea = React.forwardRef<
   HTMLTextAreaElement,
-  React.ComponentProps<"textarea">
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>
 >(({ className, ...props }, ref) => {
   return (
     <textarea
       className={cn(
-        "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
         className
       )}
       ref={ref}
@@ -17,6 +16,6 @@ const Textarea = React.forwardRef<
     />
   )
 })
-Textarea.displayName = "Textarea"
+Textarea.displayName = 'Textarea'
 
-export { Textarea }
+export { Textarea } 
